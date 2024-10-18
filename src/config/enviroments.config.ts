47e7cc616f8445d49b86d710006607d684193871
NGiulianoNissero <giulianonissero.mf@gmail.com>
2024-10-18 11:42:55 -0300

    feature(src/modules/files and src/config/cloudinary.config.ts) 'files modules and clodinary config created'

--- src/config/enviroments.config.ts
+++ src/config/enviroments.config.ts
@@ -1,5 +1,8 @@
-import {config} from 'dotenv'
+import { config } from 'dotenv';
 
-config({path:'.env'});
+config({ path: '.env' });
 
 export const POSTGRES_URL = process.env.POSTGRES_URL;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
