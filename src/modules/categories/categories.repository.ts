c102b773b6ad60cc319ebe1702126c07010ed887
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-10-18 01:10:37 -0300

    fix(src/modules/categories) 'add all's changes of categories branch'

--- src/modules/categories/categories.repository.ts
+++ src/modules/categories/categories.repository.ts
@@ -30,7 +30,7 @@ export class CategoriesRepository {
     });
 
     if (!category)
-      throw new NotFoundException("Category not Found");
+      throw new NotFoundException(`Category with uuid ${id} didn't exist.`);
 
     return category;
   }
@@ -57,7 +57,7 @@ export class CategoriesRepository {
       .returning({ id: categories.id, name: categories.name });
 
     if (updatedCategory.length == 0)
-      throw new NotFoundException("Category not Found");
+      throw new NotFoundException(`Category with uuid ${id} didn't exist.`);
 
     return updatedCategory;
   }
