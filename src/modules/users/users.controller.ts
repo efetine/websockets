859f20073e386df3301635192bcfbd0ea63967c1
Catalina135 <pablod_ferrero@hotmail.com>
2024-10-17 23:04:47 -0300

    Se inicia branch

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,11 +9,6 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
   findAll() {
     return this.usersService.findAll();
