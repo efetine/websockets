4f4675e89ab953344c2c5000947eba393f991822
Catalina135 <pablod_ferrero@hotmail.com>
2024-10-19 14:38:22 -0300

    fix json

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CategoriesModule } from './modules/categories/categories.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { FilesModule } from './modules/files/files.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailModule } from './modules/mail/mail.module';
 
 @Module({
   imports: [
@@ -16,6 +17,8 @@ import { ConfigModule } from '@nestjs/config';
     OrdersModule,
     CategoriesModule,
     AuthModule,
+    FilesModule,
+    MailModule,
   ],
 })
 export class AppModule {}
