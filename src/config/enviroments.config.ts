55068e54c457392710d608181686adae594d7ee2
Florencia <flopitaranto@hotmail.com>
2024-10-21 00:49:28 -0300

    🔧 Add PORT env

--- src/config/enviroments.config.ts
+++ src/config/enviroments.config.ts
@@ -6,3 +6,4 @@ export const POSTGRES_URL = process.env.POSTGRES_URL;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const PORT = process.env.PORT;
