265af60ca30ba028c218c024db7cf20a10e2e926
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-10-22 10:58:55 -0300

    fix(db/schemas/orders.schema) 'fix in the relations'

--- db/schemas/orders.schema.ts
+++ db/schemas/orders.schema.ts
@@ -6,6 +6,7 @@ import {
   varchar,
   uuid,
   boolean,
+  text
 } from 'drizzle-orm/pg-core';
 import { users } from './users.schema';
 import { products } from './products.schema';
@@ -20,7 +21,7 @@ export const orderStatusEnum = pgEnum('order_status', [
 export const orders = pgTable('orders', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
   mpOrderId: varchar('mp_order_id', { length: 256 }).unique(),
-  userId: uuid().references(() => users.id, { onDelete: 'cascade' }),
+  userId: text().references(() => users.id),
   orderEstatus: orderStatusEnum('order_status').default('pending'),
   isPaid: boolean('is_paid').default(false),
   createdAt: varchar('created_at', { length: 256 }).default(
@@ -33,17 +34,25 @@ export type selectOrders = typeof orders.$inferSelect;
 export const InsertOrderSchema = createInsertSchema(orders);
 
 export const ordersRelations = relations(orders, ({ one }) => ({
-  user: one(users),
-  ordersDetails: one(ordersDetails),
+  user: one(users, { fields: [orders.userId], references: [users.id] }),
+  ordersDetails: one(ordersDetails, {
+    fields: [orders.id],
+    references: [ordersDetails.orderId],
+  }),
 }));
 
 export const ordersDetails = pgTable('orders_details', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
   orderId: integer('order_id').references(() => orders.id),
-  productId: uuid('product_id').references(() => products.id),
+  productId: varchar('product_id').references(() => products.id),
   quantity: integer('quantity'),
   price: integer('price'),
 });
+
+export const ordersDetailsRelations = relations(ordersDetails,({one}) => ({
+  order: one(orders, {fields:[ordersDetails.orderId], references:[orders.id]}),
+  product: one(products, {fields:[ordersDetails.productId], references:[products.id]}),
+}))
 
 export type insertOrdersDetails = typeof ordersDetails.$inferInsert;
 export type selectOrdersDetails = typeof ordersDetails.$inferSelect;
--- db/schemas/users.schema.ts
+++ db/schemas/users.schema.ts
@@ -1,11 +1,10 @@
+import { relations } from 'drizzle-orm';
 import { boolean } from 'drizzle-orm/pg-core';
 import { text } from 'drizzle-orm/pg-core';
-import {
-  pgTable,
-  timestamp,
-} from 'drizzle-orm/pg-core';
+import { pgTable, timestamp } from 'drizzle-orm/pg-core';
 import { createInsertSchema } from 'drizzle-zod';
 import { z } from 'zod';
+import { orders } from './orders.schema';
 
 export const users = pgTable('user', {
   id: text('id')
@@ -18,6 +17,10 @@ export const users = pgTable('user', {
   image: text('image').default('default_profile_picture.png').notNull(),
   active: boolean().default(true).notNull(),
 });
+
+export const userRelations = relations(users, ({ many }) => ({
+  orders: many(orders),
+}));
 
 const insertUserSchema = createInsertSchema(users);
 export type CreateUserDto = z.infer<typeof insertUserSchema>;
