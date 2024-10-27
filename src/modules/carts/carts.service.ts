8da8f9476e2129090dd70316b4791285207e2403
NGiulianoNissero <giulianonissero.mf@gmail.com>
2024-10-27 12:14:38 -0300

    fixed 9

---
+++ src/modules/carts/carts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CartsRepository } from './carts.repository';
+
+@Injectable()
+export class CartsService {
+  constructor(private readonly cartsRepository: CartsRepository) {}
+
+  async getCartByUserId(userId: string) {
+    return await this.cartsRepository.getCartByUserId(userId);
+  }
+
+  async createOrAddCart(userId: string, product: string) {
+    return await this.cartsRepository.createOrAddCart(userId, product);
+  }
+
+  async removeProduct(userId: string, product: string) {
+    return await this.cartsRepository.removeProduct(userId, product);
+  }
+}
