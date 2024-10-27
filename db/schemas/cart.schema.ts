f65050b99e0cf4926aa6c299b43749dc11b9e9e4
NGiulianoNissero <giulianonissero.mf@gmail.com>
2024-10-27 12:07:18 -0300

    fixed 2

---
+++ db/schemas/cart.schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, text } from 'drizzle-orm/pg-core';
+import { users } from './users.schema';
+import { integer } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { cartAndProducts } from './cart_products.schema';
+
+export const carts = pgTable('carts', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull()
+    .unique(),
+});
+
+export const cartRelations = relations(carts, ({ many, one }) => ({
+  user: one(users, { fields: [carts.userId], references: [users.id] }),
+  products: many(cartAndProducts),
+}));
