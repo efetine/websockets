e3ec00103361f7af7d7befd0334b9306c36cc5e4
NGiulianoNissero <giulianonissero.mf@gmail.com>
2024-10-27 12:12:55 -0300

    fixed 6

--- db/schemas/schema.ts
+++ db/schemas/schema.ts
@@ -296,3 +296,5 @@ export * from './products.schema';
 export * from './categories.schema';
 export * from './users.schema';
 export * from './orders.schema';
+export * from './cart.schema';
+export * from './cart_products.schema';
