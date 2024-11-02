cd5fd683993bd2c1786de5179648fb5ed4537845
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-11-02 02:55:11 -0300

    fix(src/guards/auth.guard.ts) 'now the token dont give all user data, because need make a peticion for get the user data'

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -8,6 +8,9 @@ import { JwtService } from '@nestjs/jwt';
 import { getToken } from 'next-auth/jwt'
 
 import { JWT_SECRET } from '../config/enviroments.config';
+import { db } from '../config/db';
+import { eq } from 'drizzle-orm';
+import { users } from '../../db/schemas/users.schema';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
@@ -18,9 +21,13 @@ export class AuthGuard implements CanActivate {
 
       const token = await getToken({ req:request, secret:JWT_SECRET });
 
-      if (!token) throw new UnauthorizedException();
+      if (!token?.email) throw new UnauthorizedException();
 
-      request.user = token.user;
+      const user = await db.query.users.findFirst({
+        where: eq(users.email, token.email)
+      })
+
+      request.user = user;
       return true; 
     } catch (error) {
       throw new UnauthorizedException();
