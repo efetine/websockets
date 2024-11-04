caa5438e5ec9b501e2c641ef40bdd27d761ce554
Catalina135 <pablod_ferrero@hotmail.com>
2024-11-03 21:25:25 -0300

    feature(src/module/*files*products) Se finaliza configuracion de multiples imagenes

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { FilesRepository } from './files.repository';
-import { ImageEntity, InsertImage } from '../../../db/schemas/files.schema';
+import { InsertImage } from '../../../db/schemas/files.schema';
 
 @Injectable()
 export class FilesService {
@@ -27,7 +27,6 @@ export class FilesService {
   async uploadMultipleImages(files: Express.Multer.File[]) {
     const results = await Promise.allSettled(
       files.map((file) => {
-        // Verificar tamaño del archivo
         if (file.size > this.MAX_FILE_SIZE) {
           return Promise.reject(
             new Error(`File size too large. Maximum is ${this.MAX_FILE_SIZE}.`),
@@ -37,7 +36,6 @@ export class FilesService {
       }),
     );
 
-    // Filtrar archivos que fallaron y que se subieron correctamente
     const rejected = results.filter((result) => result.status === 'rejected');
     const successful = results
       .filter(
@@ -48,15 +46,13 @@ export class FilesService {
           secure_url: string;
         }> => result.status === 'fulfilled',
       )
-      .map((result) => result.value); // Extraer el value de los resultados exitosos
+      .map((result) => result.value);
 
-    // Loguear los archivos que no se pudieron subir
     if (rejected.length > 0) {
       console.log('Some images failed to upload:', rejected);
     }
 
-    // Devolver los resultados de los que se subieron correctamente
-    return successful; // Ahora es un array de objetos que contienen public_id y secure_url
+    return successful;
   }
 
   async saveImages(
@@ -66,10 +62,9 @@ export class FilesService {
     const imageData: InsertImage[] = files.map((image) => ({
       publicId: image.public_id,
       secureUrl: image.secure_url,
-      productId: productId, // Asegúrate de incluir productId aquí
+      productId: productId,
     }));
 
-    // Asegúrate de que la inserción está utilizando el esquema adecuado
     return this.filesRepository.createMultipleImages(imageData);
   }
 
